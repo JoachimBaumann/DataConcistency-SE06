9d5f2081c567dbccc80054f01398f4c2fd3d33a9
Joachim Baumann <jobau19@student.sdu.dk>
2023-04-27 11:14:21 +0200

    initial catalog spring boot

---
+++ Catalog/src/main/java/sdu/se06/Catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package sdu.se06.Catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
---
+++ Catalog/src/test/java/sdu/se06/Catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package sdu.se06.Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
