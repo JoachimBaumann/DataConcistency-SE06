bd9578ed83fb5642b1cee096b022a81d54448705
Joachim Baumann <jobau19@student.sdu.dk>
2023-05-08 15:31:26 +0200

    Added getbyID endpoint, and logic in CatalogController

--- Catalog/src/main/java/sdu/se06/Catalog/ListingController/CatalogController.java
+++ Catalog/src/main/java/sdu/se06/Catalog/ListingController/CatalogController.java
@@ -1,10 +1,13 @@
 package sdu.se06.Catalog.ListingController;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sdu.se06.Catalog.Catalog.Listing;
 
 import java.util.List;
+import java.util.Optional;
+
 @RequestMapping("/api/v1/")
 @RestController
 public class CatalogController {
@@ -15,16 +18,27 @@ public class CatalogController {
         this.repository = repository;
     }
 
-    @PostMapping("/listing/")
+    @PostMapping("/listings/")
     public ResponseEntity<String> createListing(Listing listings) {
 
         repository.save(listings);
         return ResponseEntity.ok("Data saved");
     }
 
-    @GetMapping("/listing/")
+    @GetMapping("/listings/")
     public List<Listing> getListings() {
 
         return repository.findAll();
+    }
+
+    @GetMapping(value = "/listing/{id}")
+    public ResponseEntity<Listing> getListing(@PathVariable("id") int id) {
+        Optional<Listing> listingData = repository.findById(id);
+
+        if (listingData.isPresent()) {
+            return new ResponseEntity<>(listingData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
