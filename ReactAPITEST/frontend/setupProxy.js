142f05c26c29de287dc7e41c559ef14928a75ba3
Joachim Baumann <jobau19@student.sdu.dk>
2023-05-21 13:27:09 +0200

    updated docker-compose to run the application

--- ReactAPITEST/frontend/setupProxy.js
+++ ReactAPITEST/frontend/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/images',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: 'http://reactservice:3000',
       pathRewrite: {
         '^/images': '',
       },
