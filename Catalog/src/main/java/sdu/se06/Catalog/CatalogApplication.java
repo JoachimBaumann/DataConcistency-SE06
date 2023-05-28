6f56e8810d193ef06af02f8a30be10ecb551b79d
Joachim R Baumann <joachimrbaumann@gmail.com>
2023-05-28 09:15:39 +0200

    added CatalogControllerTests

--- Catalog/src/main/java/sdu/se06/Catalog/CatalogApplication.java
+++ Catalog/src/main/java/sdu/se06/Catalog/CatalogApplication.java
@@ -1,16 +1,8 @@
 package sdu.se06.Catalog;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import sdu.se06.Catalog.Kafka.KafkaEventProducer;
-
-import java.util.List;
 @EnableKafka
 @SpringBootApplication
 public class CatalogApplication {
--- Catalog/src/test/java/sdu/se06/Catalog/CatalogApplicationTests.java
+++ Catalog/src/test/java/sdu/se06/Catalog/CatalogApplicationTests.java
@@ -1,7 +1,6 @@
 package sdu.se06.Catalog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 class CatalogApplicationTests {
