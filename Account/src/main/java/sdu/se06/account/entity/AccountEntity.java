85c3ca549978576e99f4fef8b31f799bebd5e231
Joachim R Baumann <joachimrbaumann@gmail.com>
2023-05-29 11:28:40 +0200

    added AccountControllerUnitTest

--- Account/src/main/java/sdu/se06/account/entity/AccountEntity.java
+++ Account/src/main/java/sdu/se06/account/entity/AccountEntity.java
@@ -6,8 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 //This annotation specifies this class as a JPA entity, making sure that these values are mapped to the SQL database
+@Getter @Setter
 @Entity @AllArgsConstructor
 public class AccountEntity {
     public AccountEntity() {
@@ -30,43 +33,4 @@ public class AccountEntity {
 
     private String email;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 }
